87b191bbe3ff92fc29dc5e3fb01a5efee30d424e
Davinder Singh <dave626429@gmail.com>
2022-08-04 12:43:22 +0530

    init commit

---
+++ java-spring-recipe-web/src/main/java/com/testapp/JavaSpringRecipeWebApplication.java
@@ -0,0 +1,13 @@
+package com.testapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringRecipeWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringRecipeWebApplication.class, args);
+	}
+
+}
